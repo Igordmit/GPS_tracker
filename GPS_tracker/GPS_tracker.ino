f751d802d331878fbf511e5932a82ede4ede1786
Igordmit <igorzh1992@yandex.ru>
2023-01-05 19:12:21 +1000

    Update GPS_tracker.ino
    first check

--- GPS_tracker/GPS_tracker.ino
+++ GPS_tracker/GPS_tracker.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyGPS.h>
+TinyGPS gps;
+bool newData;
+unsigned long start;
+int currentData;
 
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  Serial.println(TinyGPS::library_version());
+  newData = false;
+  start = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if ((millis() - start > 5000) && (Serial1.available())) {
+    currentData = Serial1.read();
+    Serial.println(currentData);
+    if (('\r' != currentData) && gps.encode(currentData)) {
+      Serial.println("debug");
+      newData = true;
+    }
+
+    if(newData){
+        gpsdump(gps);
+    }
+
+    start = millis();
+  }
+}
+
+void gpsdump(TinyGPS &gps){
+  long lat, lon;
+  gps.get_position(&lat, &lon);
+  Serial.print(lat); Serial.print(", "); Serial.print(lon); 
 }
